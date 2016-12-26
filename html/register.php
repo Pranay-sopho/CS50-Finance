e010f2e348fd82afa688ab6a30d18164271fc13d
Pranay Kotkar <kotkar.pranay04@gmail.com>
2016-12-26 09:29:40 -0500

    Register Completed

--- html/register.php
+++ html/register.php
@@ -13,7 +13,39 @@
     // else if user reached page via POST (as by submitting a form via POST)
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        //TODO
+        // validate submission
+        if (empty($_POST["username"]))
+        {
+            apologize("You must provide your username.");
+        }
+        else if (empty($_POST["password"]))
+        {
+            apologize("You must provide your password.");
+        }    
+        else if ($_POST["password"] != $_POST["confirmation"])
+        {
+            apologize("You must enter same password twice");
+        }
+        
+        // query database for user
+        $result = CS50::query("INSERT IGNORE INTO users (username, hash, cash) VALUES(?, ?, 10000.0000)", $_POST["username"], password_hash($_POST["password"], PASSWORD_DEFAULT));
+        
+        // if entered username exists, log into his account
+        if ($result != 0)
+        {
+            // find out the id assigned to newly registerd user
+            $rows = CS50::query("SELECT LAST_INSERT_ID() AS id");
+            $id = $rows[0]["id"];
+            
+            // remember that user's now logged in by storing user's ID in session
+            $_SESSION["id"] = $id;
+            
+            // redirect 
+            redirect("/");
+        }
+        
+        // else apologize
+        apologize("Username already exists, try another");
     }
     
 ?>
