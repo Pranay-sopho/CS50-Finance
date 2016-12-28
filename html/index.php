f08b13569dd6a9741d9e4a8e01512c040db4651b
Pranay Kotkar <kotkar.pranay04@gmail.com>
2016-12-28 09:08:47 -0500

    Portfolio Completed

--- html/index.php
+++ html/index.php
@@ -3,7 +3,29 @@
     // configuration
     require("../includes/config.php"); 
 
+    // query portfolio database for user
+    $rows = CS50::query("SELECT symbol,shares FROM portfolio WHERE user_id = ?", $_SESSION["id"]);
+    
+    // store all portfolio info of a user in positions 
+    $positions = [];
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "name" => $stock["name"],
+                "price" => number_format($stock["price"], 4),
+                "shares" => $row["shares"],
+                "symbol" => $row["symbol"]
+            ];
+        }
+    }
+    
+    // query users database for user
+    $cash = CS50::query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
+        
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["title" => "Portfolio", "positions" => $positions, "cash" => $cash]);
 
 ?>
--- views/portfolio.php
+++ views/portfolio.php
@@ -1,3 +1,19 @@
-<div>
-    <iframe allowfullscreen frameborder="0" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3&rel=0" width="420"></iframe>
-</div>
+<table style="width: 100%">
+    <tr>
+        <th>Name</th>
+        <th>Symbol</th>
+        <th>Shares</th>
+        <th>Price</th>
+    </tr>
+    <?php foreach ($positions as $position): ?>
+    
+        <tr>
+            <td><?= $position["name"] ?></td>
+            <td><?= $position["symbol"] ?></td>
+            <td><?= $position["shares"] ?></td>
+            <td><?= $position["price"] ?></td>
+        </tr>
+        
+    <?php endforeach ?>
+</table>
+<h1>You have <?= $cash ?> left.<h1>
